059b681d8bc122d6a1700a3cbc554dd13e17d307
Eduardo Licea López <eduardo.licea@c3.unam.mx>
2023-05-22 12:56:50 -0600

    más roles

---
+++ src/roles/cliente-trabajador.ts
@@ -0,0 +1,30 @@
+import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from "@nestjs/common";
+import { Observable } from "rxjs";
+import * as jwt from 'jsonwebtoken';
+import { GqlExecutionContext } from "@nestjs/graphql";
+
+
+@Injectable()
+export class TrabajadorAdminGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const ctx = GqlExecutionContext.create(context);
+    const token = ctx.getContext().req.headers.authorization?.split(' ')[1];
+
+    if (token){
+      try{
+
+        const decodedToken = jwt.verify(token, 'hide-me') as {role : string};
+        ctx.getContext().req.user = {user_role: decodedToken.role};
+        return decodedToken.role.includes('trabajador') || decodedToken.role.includes('cliente');
+
+      }catch (err){
+        throw new ForbiddenException(`Error: ${err}`);
+      }
+
+    }else{
+      throw new ForbiddenException('You do not have permission to access this resource.');
+      
+    }
+
+  }
+}
